bb9e10e09533bc4e45121a174c5a20bd7a908dbb
Hooorus <2479711422@qq.com>
2023-12-26 18:53:00 +0800

    resolve selenium problem

--- myFirstSpider/spiders/selenium_tencent_med.py
+++ myFirstSpider/spiders/selenium_tencent_med.py
@@ -21,14 +21,12 @@ class SeleniumTencentMedSpider(scrapy.Spider):
         self.driver = webdriver.Chrome()
 
     def parse(self, response, **kwargs):
+        # 父path
         response_xpath = "//div[@class='feed-item']/div[@class='feed-item-card']/div[@class='card-article']"
-        img_xpath = "//div[@class='img-wrap']/img[@class='img']"
-        title_xpath = "//div[@class='feed-doc-info']/div[@class='title']/span[@class='txt']"
-        content_xpath = ""
-        publisher_xpath = "//div[@class='feed-doc-info']/div[@class='source-wrap']/span"
-
-        # 存放爬取结果
-        # allocations = set()
+        # 子path，以./开头，为相对位置
+        img_xpath = "./div[@class='img-wrap']/img[@class='img']"
+        title_xpath = "./div[@class='feed-doc-info']/div[@class='title']/span[@class='txt']"
+        publisher_xpath = "./div[@class='feed-doc-info']/div[@class='source-wrap']/span"
 
         # 得到响应的url
         self.driver.get(response.url)
@@ -36,25 +34,23 @@ class SeleniumTencentMedSpider(scrapy.Spider):
 
         try:
             # 使用显式等待，等待元素加载完成，10秒钟
-            elements = WebDriverWait(self.driver, 10).until(
+            elements = WebDriverWait(self.driver, 5).until(
                 # presence_of_all_elements_located: 获取所有匹配此xpath的元素
                 expected_conditions.presence_of_all_elements_located((By.XPATH, response_xpath))
             )
 
-            self.log("-------------element-------------\n%s" % elements)
+            self.log("-------------elements-------------\n%s" % elements)
 
             # 暂存区
             allocation = TencentMedItem()
 
-            # 遍历所有找到的元素
+            # 遍历所有找到的元素，这里meta_element是几个不同的数据，输出就输出len(elements)个数据
             for meta_element in elements:
 
+                print("meta_element: ", type(meta_element))
+                print("elements: ", type(elements))
+
                 print("len of elements: ", len(elements))
-
-                title_container = []
-                img_container = []
-                publisher_container = []
-
 
                 # 取title
                 title_elements = meta_element.find_elements(By.XPATH, title_xpath)
@@ -69,31 +65,23 @@ class SeleniumTencentMedSpider(scrapy.Spider):
                 # 遍历title
                 for title_element in title_elements:
                     self.log("-------------title_element.text-------------\n%s" % title_element.text)
-                    title_container.append(title_element.text)
+                    allocation['title'] = title_element.text
 
                 # 遍历img
                 for img_element in img_elements:
-                    self.log("-------------img_element.get_attribute('src')-------------\n%s" % img_element.get_attribute("src"))
-                    img_container.append(img_element.get_attribute("src"))
+                    self.log(
+                        "-------------img_element.get_attribute('src')-------------\n%s" % img_element.get_attribute(
+                            "data-src"))
+                    allocation['img'] = img_element.get_attribute("data-src")
 
                 # 遍历publisher
                 for publisher_element in publisher_elements:
                     self.log("-------------publisher_element.text-------------\n%s" % publisher_element.text)
-                    publisher_container.append(publisher_element.text)
+                    allocation['publisher'] = publisher_element.text
 
-                print("title_container: \n", title_container)
-                print("img_container: \n", img_container)
-                print("publisher_container: \n", publisher_container)
+                print("allocation: ", allocation)
 
-
-                # self.log("-------------element get_attribute('src')-------------\n%s" % meta_element.get_attribute("src"))
-                # allocation['img'] = meta_element.get_attribute("src")
-
-                allocation['title'] = title_container
-                allocation['img'] = img_container
-                allocation['publisher'] = publisher_container
-
-            return allocation
+                yield allocation
 
         except Exception as e:
             print(f"Error: {e}")
