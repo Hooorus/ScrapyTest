a9ec052be8be4ba4ec60db4c62b31df2048f031c
Hooorus <2479711422@qq.com>
2023-12-29 18:24:52 +0800

    rename lym_scrapy to nine_nine_com_cn

--- nine_nine_com_cn/myFirstSpider/spiders/selenium_tencent_med.py
+++ nine_nine_com_cn/myFirstSpider/spiders/selenium_tencent_med.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
-from lym_scrapy.myFirstSpider.items import TencentMedItem
+from nine_nine_com_cn.myFirstSpider.items import TencentMedItem
 
 
 class SeleniumTencentMedSpider(scrapy.Spider):
